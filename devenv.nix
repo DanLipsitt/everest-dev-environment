84fc9b3672aed783ce34b83f96256e2d76095f5a
Dan Lipsitt <578773+DanLipsitt@users.noreply.github.com>
2024-04-24 23:20:56 -0700

    Cleanup.

--- devenv.nix
+++ devenv.nix
@@ -1,29 +1,14 @@
 { pkgs, lib, config, inputs, ... }:
 
 let
-  # build the xdgappdirs package from pip
   xdgappdirs = pkgs.python311Packages.buildPythonPackage rec {
-
     pname = "xdgappdirs";
     version = "1.4.5";
-      format = "wheel";
-
+    format = "wheel";
     src = pkgs.fetchPypi {
       inherit pname version format;
-      # python = "py2.py3";
-      # dist = python;
-      # platform = "any";
       sha256 = "sha256-j6S0cwSd5brdMdyYhhzxavbP8oG1F46ZdVVivLXwzYg=";
     };
-
-    # The "native" dependency isn't used by the package, it's just used during
-    # the build process to create the Python package.
-    # nativeBuildInputs = with pkgs; [
-    #   python3.pkgs.wrapPython
-    # ];
-
-    # We need the python interpreter to build the package.
-    #pythonImportsCheck = [ "xdgappdirs" ];
   };
   in
 {
@@ -38,7 +23,7 @@ let
   enterShell = ''
     # set CPM_SOURCE_CACHE using the xdgappdirs python package if not set
     if [ -z ''${CPM_SOURCE_CACHE} ]; then
-      CPM_SOURCE_CACHE=<(python -c 'import xdgappdirs; print(xdgappdirs.user_cache_dir())')
+      CPM_SOURCE_CACHE=$(python -c 'import xdgappdirs; print(xdgappdirs.user_cache_dir()')
     fi
     export CPM_SOURCE_CACHE
   '';
